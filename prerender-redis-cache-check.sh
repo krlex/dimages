f17f2a8e78dc6707cf68c3d9ce8f19e9672cd366
krlex <krle@tilda.center>
2017-03-07 13:22:39 +0100

    change destination of server.js

--- prerender-redis-cache-check.sh
+++ prerender-redis-cache-check.sh
@@ -1,9 +1,9 @@
-if grep -q "prerender-redis-cache" server.js
+if grep -q "prerender-redis-cache" /prerender/server.js
     then
 
      echo "prerender-redis-cache exist";
 
     else
 
-     sed -i '9 i\server.use(require('prerender-redis-cache'));' server.js
+     sed -i '9 i\server.use(require('prerender-redis-cache'));' /prerender/server.js
 fi
